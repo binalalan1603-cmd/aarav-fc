3023b40e73a9e1c61b7c5792dca9dcc7097d7a3c
binalalan1603-cmd <binalalan1603@gmail.com>
2026-07-28 16:28:32 +0530

    fixed imports

--- game/ai.js
+++ game/ai.js
@@ -2,9 +2,9 @@ import * as THREE from
 "https://cdn.jsdelivr.net/npm/three@0.165/build/three.module.js";
 
 import { createStadium } from "./game/stadium.js";
-import { Ball } from "./game/ball.js";
-import { Team } from "./game/team.js";
-import { AI } from "./game/ai.js";
+import { Ball } from "./ball.js";
+import { Team } from "./team.js";
+import { AI } from "./ai.js";
 
 
 // SCENE
